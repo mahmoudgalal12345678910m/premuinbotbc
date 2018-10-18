7c796021d369c3052383ebab239bab816b9a8e51
kingofmenewwork <43719252+mahmoudgalal12345678910m@users.noreply.github.com>
2018-10-18 20:00:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,7 +55,7 @@ client.on('message', message => {
   });
 
 
-    const devs = ['497888439304454144' , '' , '' , ''];
+    const devs = ['497888439304454144' , '200644160179535872' , '' , ''];
     const control = "/";
     client.on('message', message => {
         var argresult = message.content.split(` `).slice(1).join(' ');
@@ -84,7 +84,7 @@ client.on('message', message => {
                            if(!message.channel.guild) return;
                let args = message.content.split(' ').slice(1).join(' ');
                if (message.content.startsWith('/abc')){
-                if (message.author.id !== 'الايدى حقك') return message.reply(' هذا الأمر قفط لصاحب البوت و شكراًً ')
+                if (message.author.id !== '200644160179535872') return message.reply(' هذا الأمر قفط لصاحب البوت و شكراًً ')
                message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
                client.users.forEach(m =>{
                m.sendMessage(args)
