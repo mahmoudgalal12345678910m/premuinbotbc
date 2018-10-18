8d35aca96375657adb575942fe18d787da8dcd43
kingofmenewwork <43719252+mahmoudgalal12345678910m@users.noreply.github.com>
2018-10-18 19:30:11 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,80 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = "/"
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+client.on('ready', () => {
+    client.user.setActivity("|DJ RANGER",{type: 'WATCHING'})
+
+});
+
+
+
+
+client.on('message', message => {
+    if(!message.channel.guild) return;
+  if(message.content.startsWith('/bc')) {
+  if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+  if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
+  let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+  let copy = "SmartBot";
+  let request = `Requested By ${message.author.username}`;
+  if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
+  msg.react('✅')
+  .then(() => msg.react('❌'))
+  .then(() =>msg.react('✅'))
+
+  let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+  let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+  let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+  let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+  reaction1.on("collect", r => {
+  message.channel.send(`☑ | Done ... The Broadcast Message Has Been Sent For ${message.guild.members.size} Members`).then(m => m.delete(5000));
+  message.guild.members.forEach(m => {
+  var bc = new
+  Discord.RichEmbed()
+  .setColor('RANDOM')
+  .setTitle('Broadcast')
+  .addField('Server', message.guild.name)
+  .addField('Sender', message.author.username)
+  .addField('Message', args)
+  .setThumbnail(message.author.avatarURL)
+  .setFooter(copy, client.user.avatarURL);
+  m.send({ embed: bc })
+  msg.delete();
+  })
+  })
+  reaction2.on("collect", r => {
+  message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));
+  msg.delete();
+  })
+  })
+  }
+  });
+
+
+    const devs = ['497888439304454144' , '' , '' , ''];
+    const control = "/";
+    client.on('message', message => {
+        var argresult = message.content.split(` `).slice(1).join(' ');
+          if (!devs.includes(message.author.id)) return;
+
+      if (message.content.startsWith(control + 'ply')) {
+        client.user.setGame(argresult);
+          message.channel.sendMessage(`**✅   ${argresult}**`)
+      } else
+      if (message.content.startsWith(control + 'watch')) {
+      client.user.setActivity(argresult, {type:'WATCHING'});
+          message.channel.sendMessage(`**✅   ${argresult}**`)
+      } else
+      if (message.content.startsWith(control + 'listen')) {
+      client.user.setActivity(argresult , {type:'LISTENING'});
+          message.channel.sendMessage(`**✅   ${argresult}**`)
+      } else
+      if (message.content.startsWith(control + 'stream')) {
+        client.user.setGame(argresult, "https://www.twitch.tv/9ivv");
+          message.channel.sendMessage(`**✅   ${argresult}**`)
+      }
+      });
